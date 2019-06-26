62d981d5bb4e5513f135278fb9611e34bc75b065
zzb <zzbainiding.com>
2019-06-26 19:13:14 +0800

    20190626

--- springboot-service/src/main/java/com/myapp/core/SpringbootServiceApplication.java
+++ springboot-service/src/main/java/com/myapp/core/SpringbootServiceApplication.java
@@ -1,11 +1,13 @@
 package com.myapp.core;
 
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableDubboConfiguration
+@MapperScan("com.myapp.core.mapper")
 public class SpringbootServiceApplication {
 
     public static void main(String[] args) {
--- springboot-service/src/main/java/com/myapp/core/service/impl/HelloAServiceImpl.java
+++ springboot-service/src/main/java/com/myapp/core/service/impl/HelloAServiceImpl.java
@@ -1,7 +1,10 @@
 package com.myapp.core.service.impl;
 
+import com.myapp.core.mapper.IUserDao;
 import com.myapp.core.service.HelloAService;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * @author :zzb
@@ -10,9 +13,14 @@ import org.springframework.stereotype.Service;
  */
 @Service("helloAService")
 public class HelloAServiceImpl implements HelloAService {
+
+    @Resource
+    private IUserDao userDao;
+
     @Override
     public String sayHello(String name) {
         System.out.println("----------------------002-----------------执行BIZ层service--------------------------------");
+        userDao.addUser("zzb0001","zzb0001");
         return name+"-------Service";
     }
 }
